d30d531a9ed8ce7ec748f2ebf97692d6f52c4cef
bingisatish <50958671+bingisatish@users.noreply.github.com>
2025-06-30 13:34:43 +0530

    Update Program.cs
    The 500 error was caused by a missing service registration. Your UserController requires IMemoryCache but it wasn't registered in the dependency injection container. I've added builder.Services.AddMemoryCache(); to fix this.

--- ReactJS-WinAuth/Program.cs
+++ ReactJS-WinAuth/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 // Add Windows Authentication
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
